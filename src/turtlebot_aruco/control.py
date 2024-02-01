6fcfb15fc3042e9cca7648bc5ef85cec0410f681
18PatZ <18PatZ@students.harker.org>
2024-01-31 16:31:04 -0800

    Arc driving and sync waggling

--- src/turtlebot_aruco/control.py
+++ src/turtlebot_aruco/control.py
@@ -8,9 +8,13 @@ from geometry_msgs.msg import Vector3
 
 from turtlebot_aruco.msg import Aruco
 
-import time
+import cv2
 
-TURN_DIRECTION = -1
+import time
+import math
+import numpy as np
+
+TILE_SIZE = 0.2286
 
 found_marker = None
 pub = None
@@ -20,21 +24,30 @@ original_yaw = None
 
 sub = None
 
+
 def aruco_callback(aruco_msg):
     global found_marker
     if aruco_msg.has_marker:
         found_marker = aruco_msg
 
 
-def set_velocity(control_angular_vel):
+def set_velocity(control_linear_vel, control_angular_vel):
     twist = Twist()
 
-    twist.linear.x = twist.linear.y = twist.linear.z = 0.0
+    twist.linear.y = twist.linear.z = 0.0
     twist.angular.x = twist.angular.y = 0.0
 
+    
+    twist.linear.x = control_linear_vel
     twist.angular.z = control_angular_vel
 
     pub.publish(twist)
+
+    print("Velocity FORWARD",control_linear_vel, "ROTATE", control_angular_vel) 
+
+
+def set_turn_velocity(control_angular_vel):
+    set_velocity(0.0, control_angular_vel)
 
 
 def get_yaw():
@@ -56,11 +69,11 @@ def turn_to_marker(direction):
 
     print("Camera engaged. Beginning turn...")
 
-    set_velocity(0.1 * direction)
+    set_turn_velocity(0.1 * direction)
     while found_marker is None:
         time.sleep(0.01)
 
-    set_velocity(0.0)
+    set_turn_velocity(0.0)
     
     print("Found marker! Obtaining yaw...")
     time.sleep(1)
@@ -93,9 +106,129 @@ def turn_to_marker(direction):
         time.sleep(0.1)
 
     pub_pid_enabled.publish(False)
-    set_velocity(0.0)
-
+    set_turn_velocity(0.0)
+    time.sleep(0.5)
+    set_turn_velocity(0.0)
+    
     print("Turn complete. Current yaw", get_yaw(), "vs original", original_yaw)
+    found_marker = None
+
+
+
+
+
+
+def synchronization_turn(direction):
+
+    period = 4
+    sp = 0.2
+
+    set_turn_velocity(sp * direction)
+    time.sleep(period/2)
+
+    set_turn_velocity(-sp * direction)
+    time.sleep(period)
+
+    set_turn_velocity(sp * direction)
+    time.sleep(period)
+
+    set_turn_velocity(-sp * direction)
+    time.sleep(period/2)
+
+    set_turn_velocity(0.0)
+    
+    print("Sync complete.")
+
+
+def watch_for_synchronization(direction):
+
+    subscribe_aruco()
+
+    target_start_yaw = None
+    last_measurement = None
+    stage = 0
+
+
+    last = time.time()
+
+    while True:
+        if found_marker is not None:
+            r = found_marker.marker_rotation
+            rot_vec = np.array([r.x, r.y, r.z])
+
+            rot_mat, _ = cv2.Rodrigues(rot_vec)
+            
+            # rotate Z vector to align with marker. Marker's Z is normal to it
+            out_of_marker = rot_mat.dot(np.array([0,0,1])) 
+            out_of_marker /= np.linalg.norm(out_of_marker)
+
+            marker_vec = np.array([out_of_marker[0], out_of_marker[2]])
+            
+            angle = math.atan2(marker_vec[0], marker_vec[1]) * 180/math.pi
+            # print(angle)
+
+            
+            if target_start_yaw is None:
+                target_start_yaw = angle
+                last_measurement = 0
+
+            diff = ang_diff(angle, target_start_yaw)
+            print(diff)
+
+            if stage == 0: # stationary
+                if diff - last_measurement >= 10:
+                    stage = 1
+                    last_measurement = diff
+                    print("RIGHT 1!")
+                    last = time.time()
+            elif stage == 1:  # turning to the right
+                if diff - last_measurement >= 2:
+                    last_measurement = diff
+                    last = time.time()
+                if diff - last_measurement <= -10:
+                    stage = 2
+                    last_measurement = diff
+                    print("LEFT 2!")
+            elif stage == 2:  # turning to the left
+                if diff - last_measurement <= -2:
+                    last_measurement = diff
+                    last = time.time()
+                if diff - last_measurement >= 10:
+                    stage = 3
+                    last_measurement = diff
+                    print("RIGHT 3!")
+            elif stage == 3:  # turning to the right
+                if diff - last_measurement >= 2:
+                    last_measurement = diff
+                    last = time.time()
+                if diff - last_measurement <= -10:
+                    stage = 4
+                    last_measurement = diff
+                    last = time.time()
+                    print("LEFT 4!")
+            elif stage == 4:  # turning to the left
+                if diff - last_measurement <= -2:
+                    last_measurement = diff
+                    last = time.time()
+                if diff - last_measurement >= 10:
+                    stage = 0
+                    print("RESET")
+                    last = time.time()
+                    target_start_yaw = angle
+                    last_measurement = 0
+                elif (time.time() - last) >= 2:
+                    print("COMPLETE.")
+                    break
+
+            if (time.time() - last) >= 10: # reset
+                print("RESET")
+                last = time.time()
+                target_start_yaw = angle
+                last_measurement = 0
+
+        time.sleep(0.1)
+
+    unsubscribe_aruco()
     
 
 
@@ -111,10 +244,76 @@ def unsubscribe_aruco():
     if sub is not None:
         sub.unregister()
         sub = None
-    
+  
+def drive_straight():
+    tile_size = 0.2286
+    speed = 0.2
+    set_velocity(speed, 0.0)
+    time.sleep(5 * tile_size / speed)
+    set_velocity(0.0, 0.0)
+
+
+def drive_arc(x, y, speed):
+
+    # x forwards, y left
+    # r^2 = x^2 + (r-y)^2
+    # r^2 = x^2 + r^2 - 2ry + y^2
+    # 2ry = x^2 + y^2
+    # r = (x^2 + y^2) / (2y)
+
+
+    x *= TILE_SIZE
+    y *= TILE_SIZE
+
+    if y == 0:
+        distance = abs(x)
+        angle = 0
+        r = 0
+    else:
+        r = abs((x**2 + y**2) / (2 * y))
+
+        if abs(x) >= abs(y):
+            if r == y:
+                angle = math.copysign(math.pi/2.0, y)
+            else:
+                if y >= 0:
+                    angle = math.atan(x / (r-y))
+                else:
+                    angle = -math.atan(x / (r-(-y)))
+        else:
+            if y >= 0:
+                angle = math.pi - math.atan(x / (y-r))
+            else:
+                angle = -math.pi + math.atan(x / (-y-r))
+
+        distance = abs(angle * r)
+
+    time_to_travel = abs(distance / speed)
+
+    angular_velocity = angle / time_to_travel
+
+    print(distance, angle, r, speed, time_to_travel, angular_velocity)
+
+    set_velocity(speed, angular_velocity)
+    time.sleep(time_to_travel)
+    set_velocity(0.0, 0.0)
+
+    # turn back to forward
+    # turn_speed = math.copysign(0.1, -angular_velocity)
+    # time_to_turn = -angle / turn_speed
+    time_to_turn = 1.0
+    turn_speed = -angle / time_to_turn
+
+    set_velocity(0.0, turn_speed)
+    time.sleep(time_to_turn)
+    set_velocity(0.0, 0.0)
+
 
 if __name__=="__main__":
     rospy.init_node('turtlebot_mdp_control')
+
+    agent_id = rospy.get_param('~agent_id')
+
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     pub_pid_yaw = rospy.Publisher('/pid/target_yaw', Float64, queue_size=1, latch=True)
     pub_pid_enabled = rospy.Publisher('/pid/enabled', Bool, queue_size=1, latch=True)
@@ -122,13 +321,59 @@ if __name__=="__main__":
     pub_reset = rospy.Publisher('/reset', Empty, queue_size=1)
 
     pub_pid_enabled.publish(False)
-    set_velocity(0.0)
+    set_velocity(0.0, 0.0)
     print("Holding one second for PID to halt...")
     time.sleep(1)
-    set_velocity(0.0)
+    set_velocity(0.0, 0.0)
 
     print("Obtaining current yaw...")
     original_yaw = get_yaw()
 
-    print("Started at yaw", original_yaw,". Engaging camera...")
-    turn_to_marker(-1 * TURN_DIRECTION)
+    # print("Driving forward one...")
+    # drive_straight()
+    # print("Drive complete.")
+
+    # set_velocity(0.1, 0)
+    # time.sleep(TILE_SIZE)
+    # set_velocity(0.2, 0)
+    # time.sleep(TILE_SIZE)
+    # remember wheels cant spin up to 0.2 immediately
+
+    if agent_id == 0:
+        watch_for_synchronization(-1)
+    else:
+        synchronization_turn(-1)
+
+    # print("Driving arc...")
+    # drive_arc(2, 1, 0.05)
+    # print("Drive complete.")
+
+    # print("Driving arc...")
+    # drive_arc(1, -2, 0.1)
+    # print("Drive complete.")
+
+    # print("Driving arc...")
+    # drive_arc(2, 2, 0.1)
+    # print("Drive complete.")
+
+    # time.sleep(2)
+
+    # print("Driving arc...")
+    # drive_arc(-6, -1, -0.2)
+    # print("Drive complete.")
+
+    # print("Started at yaw", original_yaw,".")
+    # print("Engaging camera...")
+
+    # if agent_id == 0:
+    #     turn_direction = -1
+    # elif agent_id == 1:
+    #     turn_direction = 1
+    # turn_to_marker(turn_direction)
+
+
+    # print("Driving arc...")
+    # drive_arc(-2, -1, -0.1)
+    # print("Drive complete.")
+
+    # turn_to_marker(turn_direction)
--- src/turtlebot_aruco/zed.py
+++ src/turtlebot_aruco/zed.py
@@ -1,5 +1,205 @@
 #!/usr/bin/env python3
 
+
+
+
+
+
+
+# The following code is used to watch a video stream, detect a Charuco board, and use
+# it to determine the posture of the camera in relation to the plane
+# of markers.
+#
+# Assumes that all markers are on the same plane, for example on the same piece of paper
+#
+# Requires camera calibration (see the rest of the project for example calibration)
+
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+import os
+import pickle
+
+
+def drawArucoMarkers(img, corners, ids):
+    if len(corners) > 0:
+        ids = ids.flatten()
+
+        for (markerCorner, markerID) in zip(corners, ids):
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+
+            # convert each of the (x, y)-coordinate pairs to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            # draw the bounding box of the ArUCo detection
+            cv2.line(img, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(img, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(img, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(img, bottomLeft, topLeft, (0, 255, 0), 2)
+
+            # compute and draw the center (x, y)-coordinates of the ArUco
+            # marker
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(img, (cX, cY), 4, (0, 0, 255), -1)
+
+            # draw the ArUco marker ID on the image
+            cv2.putText(img, str(markerID),
+                (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+    return img
+
+
+def drawProjected(img, objectPoints, rvec, tvec, cameraMatrix, distCoeffs, color, thickness):
+    projected, _ = cv2.projectPoints(objectPoints = np.array(objectPoints), rvec=rvec, tvec=tvec, cameraMatrix=cameraMatrix, distCoeffs=distCoeffs)
+
+    points = np2list(projected)
+
+    for i in range(len(points) - 1):
+        p1 = points[i]
+        p2 = points[i+1]
+        cv2.line(img, np2cvi(p1), np2cvi(p2), color, thickness)
+
+    return img
+
+
+def flatten(arr):
+    return np.array(arr).flatten()
+
+def np2cvi(p):
+    return (int(p[0]), int(p[1]))
+
+def np2list(projected_to_cam):
+    return [p[0] for p in projected_to_cam]
+
+
+
+class ArucoDetector:
+
+    def __init__(self, cameraMatrix, distCoeffs):
+        self.cameraMatrix = cameraMatrix
+        self.distCoeffs = distCoeffs
+
+        # Check for camera calibration data
+        if cameraMatrix is None or distCoeffs is None:
+            print("Calibration issue. Camera and distortion matrix must be provided!")
+            exit()
+
+        # Constant parameters used in Aruco methods
+        # ARUCO_PARAMETERS = aruco.DetectorParameters()
+        # ARUCO_DICT = aruco.getPredefinedDictionary(aruco.DICT_5X5_1000)
+        self.ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+        self.ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_1000)
+
+        # self.detector = aruco.ArucoDetector(ARUCO_DICT, ARUCO_PARAMETERS)
+
+        self.markerLength = markerLength = 54#50.8#(18.83 * 16/20) / 1000#0.05;
+        self.objPoints = np.array([
+            [-markerLength/2, markerLength/2, 0],
+            [markerLength/2, markerLength/2, 0],
+            [markerLength/2, -markerLength/2, 0],
+            [-markerLength/2, -markerLength/2, 0]
+        ])
+
+
+    def process(self, QueryImg, target_id):
+
+        target = None
+        
+        # grayscale image
+        gray = cv2.cvtColor(QueryImg, cv2.COLOR_BGR2GRAY)
+    
+        # Detect Aruco markers
+        # corners, ids, rejectedImgPoints = self.detector.detectMarkers(gray)
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.ARUCO_DICT, parameters=self.ARUCO_PARAMETERS)
+    
+        # # Refine detected markers
+        # # Eliminates markers not part of our board, adds missing markers to the board
+        # corners, ids, rejectedImgPoints, recoveredIds = self.detector.refineDetectedMarkers(
+        #         image = gray,
+        #         board = self.CHARUCO_BOARD,
+        #         detectedCorners = corners,
+        #         detectedIds = ids,
+        #         rejectedCorners = rejectedImgPoints,
+        #         cameraMatrix = self.cameraMatrix,
+        #         distCoeffs = self.distCoeffs)  
+
+        if corners is not None and len(corners) > 0:
+           
+            # Outline all of the markers detected in our image
+            QueryImg = drawArucoMarkers(QueryImg, corners, ids)
+            QueryImg = aruco.drawDetectedMarkers(QueryImg, corners, borderColor=(0, 0, 255))
+
+            #single_rvecs, single_tvecs = aruco.estimatePoseSingleMarkers(corners, 1, cameraMatrix, distCoeffs)  
+            
+            for (corner, id) in zip(corners, ids):
+                retval, rvec, tvec = cv2.solvePnP(
+                    objectPoints=self.objPoints, 
+                    imagePoints=corner, 
+                    cameraMatrix=self.cameraMatrix, 
+                    distCoeffs=self.distCoeffs)           
+                QueryImg = cv2.drawFrameAxes(QueryImg, self.cameraMatrix, self.distCoeffs, rvec, tvec, self.markerLength/2)#0.015)
+
+                data = (corner, rvec, tvec)
+                if id == target_id:
+                    target = data
+                    print("RVec:", rvec)
+                    print("TVec:", tvec)
+
+        marker_rotation = [float('NaN'), float('NaN'), float('NaN')]
+        marker_translation = [float('NaN'), float('NaN'), float('NaN')]
+
+        if target is not None:
+            marker_rotation = flatten(target[1])
+            marker_translation = flatten(target[2])
+
+            stepX = stepY = 10.8
+
+            gridSize = 10
+
+            sizeX = sizeY = gridSize * stepX
+
+            for i in range(gridSize+1):
+                x = i * stepX - sizeX/2
+                # draw y line
+                drawProjected(QueryImg, [
+                    [x, -sizeY/2, 0],
+                    [x, sizeY/2, 0],
+                ], rvec, tvec, self.cameraMatrix, self.distCoeffs, (0, 255, 255), 1)
+
+                y = i * stepY - sizeY/2
+                # draw x line
+                drawProjected(QueryImg, [
+                    [-sizeX/2, y, 0],
+                    [sizeX/2, y, 0],
+                ], rvec, tvec, self.cameraMatrix, self.distCoeffs, (0, 255, 255), 1)
+
+        has_marker = target is not None
+        return has_marker, marker_rotation, marker_translation, QueryImg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import rospy 
 from std_msgs.msg import String 
 from sensor_msgs.msg import Image, CameraInfo
@@ -14,7 +214,7 @@ from cv_bridge import CvBridge, CvBridgeError
 import time
 
 import numpy as np
-from turtlebot_aruco.turtle_aruco_legacy import ArucoDetector
+#from turtlebot_aruco.turtle_aruco_legacy import ArucoDetector
 
 
 # Initialize the CvBridge class
